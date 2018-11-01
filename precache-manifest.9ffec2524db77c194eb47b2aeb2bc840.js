16ca9052f7e198c4b85ad78ba2c404c37f0423dc
mag16 <marcoguzman16@gmail.com>
2018-10-31 22:50:12 -0700

    Updates

---
+++ precache-manifest.9ffec2524db77c194eb47b2aeb2bc840.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "c8775f842261527161eb",
+    "url": "/Conways-Life/static/js/main.c8775f84.chunk.js"
+  },
+  {
+    "revision": "21205516625e38ad464a",
+    "url": "/Conways-Life/static/js/1.21205516.chunk.js"
+  },
+  {
+    "revision": "c8775f842261527161eb",
+    "url": "/Conways-Life/static/css/main.1f4913be.chunk.css"
+  },
+  {
+    "revision": "129b30cfcf364cce58af4011934b73b6",
+    "url": "/Conways-Life/index.html"
+  }
+];
